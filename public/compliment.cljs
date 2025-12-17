f5edf636ece0064215ef7f518fae4ff0953fe9ce
Josh Glover <jmglov@jmglov.net>
2025-12-17 18:09:52 +0100

    Basic compliment generator

--- public/compliment.cljs
+++ public/compliment.cljs
@@ -1,3 +1,39 @@
 (ns igeday.compliment)
 
-(js/console.log "Hello, IGEDay!")
+(defonce state (atom {}))
+
+(def compliments
+  ["You're amazing!"
+   "You're so smart!"
+   "You're the best!"
+   "You're nice!"])
+
+(defn get-el [selector]
+  (if (string? selector)
+    (js/document.querySelector selector)
+    selector))
+
+(defn show-compliment! [el compliment]
+  (set! (.-innerText el) compliment))
+
+(defn get-compliment [compliments-list]
+  (->> (shuffle compliments-list) first))
+
+(defn handle-compliment-button [ev]
+  (->> (get-compliment compliments)
+       (show-compliment! (get-el "#compliment"))))
+
+(defn init-ui! []
+  (.addEventListener (get-el "#get-compliment") "click"
+                     handle-compliment-button)
+  (js/console.log "Hello, IGEDay!"))
+
+(when-not (:loaded @state)
+  (init-ui!)
+  (swap! state assoc :loaded true))
+
+(comment
+
+  (init-ui!)
+
+  )
