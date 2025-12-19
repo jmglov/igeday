f5ee86f7d508fac4afe3a41cfba9a8eff73c5e44
Josh Glover <jmglov@jmglov.net>
2025-12-19 09:20:50 +0100

    mood: add colour effect

--- public/mood.cljs
+++ public/mood.cljs
@@ -3,12 +3,20 @@
 
 (defonce state (atom {:counters {}}))
 
-(def moods [:happy :tired :excited :chill])
+(def moods {:happy {:color "yellow"}
+            :tired {:color "dimgrey"}
+            :excited {:color "magenta"}
+            :chill {:color "deepskyblue"}})
+
+(def color-transition-ms 250)
 
 (defn get-el [selector]
   (if (string? selector)
     (js/document.querySelector selector)
     selector))
+
+(defn set-body-color! [color]
+  (set! (.-style (get-el "body")) (str "background-color: " color)))
 
 (defn count-mood! [mood]
   (swap! state update-in [:counters mood] #(inc (or % 0)))
@@ -16,10 +24,12 @@
         el (get-el sel)
         new-count (get-in @state [:counters mood])]
     (js/console.log "Updating" (name mood) "counter to" new-count el)
-    (set! (.-innerText el) new-count)))
+    (set-body-color! (get-in moods [mood :color]))
+    (set! (.-innerText el) new-count)
+    (js/window.setTimeout #(set-body-color! "transparent") color-transition-ms)))
 
 (defn add-listeners! []
-  (doseq [mood moods
+  (doseq [mood (keys moods)
           :let [sel (str "#button-" (name mood))]]
     (.addEventListener (get-el sel) "click"
                        (fn [_ev]
